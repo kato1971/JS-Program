417561fb8d0a6caa26d28c1a7905a2d29b12aab1
kato1971 <88402289+kato1971@users.noreply.github.com>
2021-12-22 13:31:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello Winc Academy Students");
+// "This is a large sum"
+/*let multiplication = 1230941 * 1823794;
+console.log(multiplication);
+let division = 637263 / 54;
+console.log(division)*/
+// Comments are not visible in the console
